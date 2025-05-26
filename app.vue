f84ed5f03b1fe017808813ca7d92a5be59349a08
Tesar Chandra Esnawan <115932574+tesarchndr@users.noreply.github.com>
2025-05-26 11:45:22 +0700

    DONE

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@ useHead({
   meta: [
     {
       name: 'viewport',
-      content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+      content: 'width=device-width, initial-scale=1.0, maximum-scale=0 user-scalable=no'
     }
   ]
 })
