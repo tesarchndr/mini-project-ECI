1f9529efd17a12676b88dc26fc0f065f3cee69ca
Tesar Chandra Esnawan <115932574+tesarchndr@users.noreply.github.com>
2025-05-26 11:36:35 +0700

    DONE

--- app.vue
+++ app.vue
@@ -9,6 +9,12 @@ useHead({
       rel: 'stylesheet',
       href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap'
     }
+  ],
+  meta: [
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+    }
   ]
 })
 </script>
